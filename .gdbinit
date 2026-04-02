8a31234d41c061ff42c5bb1ce567c16d5b551a00
Eren Gazioglu <eren.g94@gmail.com>
2026-04-02 14:54:34 +0200

    fix randomize max value

--- .gdbinit
+++ .gdbinit
@@ -1,2 +1,6 @@
 b main
+run
+call (int)open("/dev/pts/5", 1, 0)
+call (int)dup2($1, 1)
+call (int)close($1)
 layout src
--- src/stack_random.c
+++ src/stack_random.c
@@ -6,7 +6,7 @@
 /*   By: egaziogl <egaziogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/01 16:58:23 by egaziogl          #+#    #+#             */
-/*   Updated: 2026/04/02 13:04:43 by egaziogl         ###   ########.fr       */
+/*   Updated: 2026/04/02 14:54:17 by egaziogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,16 +28,18 @@ void	empty_stack(t_stack *stack)
 
 void	fill_random(t_stack *stack, int size)
 {
-	int		val;
+	int	val;
+	int	max;
 
 	empty_stack(stack);
+	max = size * 5;
 	while (size--)
 	{
-		val = rand_max(size * 5);
+		val = rand_max(max);
 		if (stack->count)
 		{
 			while (seek(stack->top, val, false) != -1)
-				val = rand_max(size * 5);
+				val = rand_max(max);
 		}
 		add_top(stack, val);
 	}
