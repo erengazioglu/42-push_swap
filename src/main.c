a6830c89e8e9b2b5bf8d9dd3348e8c13c1c5fcb8
Eren <eren.g94@gmail.com>
2026-04-02 12:52:05 +0200

    clean up main

--- src/main.c
+++ src/main.c
@@ -6,53 +6,24 @@
 /*   By: egaziogl <egaziogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/29 23:45:58 by egaziogl          #+#    #+#             */
-/*   Updated: 2026/04/01 18:43:06 by egaziogl         ###   ########.fr       */
+/*   Updated: 2026/04/02 12:04:04 by egaziogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
-#include "time.h"
-
-void	sort_and_insert(t_state *state)
-{
-	int	dist[2];
-
-	dist[0] = distance_from_top(state->b, state->a->top->val);
-	dist[1] = distance_from_bottom(state->b, state->a->top->val);
-	if (dist[0] < dist[1])
-	{
-		while (dist[1]--)
-			do_rotate(state, false, true);
-		do_push(state, true);
-	}
-	else
-	{
-		while (dist[0]--)
-			do_rotate_reverse(state, false, true);
-		do_push(state, true);
-	}
-	print_stacks(state);
-}
 
 int	main(int argc, char **argv)
 {
 	t_state	*state;
 
 	state = init(argc, argv);
-	srand(time(NULL));
-	fill_random(state->a, 500);
-	empty_stack(state->b);
-	state->moves = 0;
-	begin(state);
-	print_stacks(state);
-	while (state->a->count)
-		sort_and_insert(state);
-	while (state->b->top->val != state->b->max)
-		do_rotate(state, false, true);
-	print_stacks(state);
-	while (state->b->count)
-		do_push(state, false);
-	print_stacks(state);
-	ft_printf("total moves: %d\n", state->moves);
+	randomize(state, 10, true);
+	for (int i = 0; i < 5; i++)
+	{
+		randomize(state, 10, false);
+		print_stacks(state);
+		test_find_cheapest(state);
+
+	}
 	return (0);
 }
--- src/tests.c
+++ src/tests.c
@@ -6,11 +6,12 @@
 /*   By: egaziogl <egaziogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/24 22:46:29 by egaziogl          #+#    #+#             */
-/*   Updated: 2026/04/01 14:24:07 by egaziogl         ###   ########.fr       */
+/*   Updated: 2026/04/02 12:01:58 by egaziogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
+#include "time.h"
 
 void	test_pop_push(t_state *state)
 {
@@ -63,4 +64,53 @@ void	test_min_max(t_state *state)
 {
 	ft_printf("min: %d\n", find_min(state->a));
 	ft_printf("max: %d\n", find_max(state->a));
+}
+
+void	test_naive_solve(t_state *state)
+{
+	int	index;
+	
+	fill_random(state->a, 10);
+	empty_stack(state->b);
+	state->moves = 0;
+	begin(state);
+	print_stacks(state);
+	index = 0; // replace with cheapest index
+	while (state->a->count)
+		sort_and_insert(state, index);
+	while (state->b->top->val != state->b->max)
+		do_rotate(state, false, true);
+	print_stacks(state);
+	while (state->b->count)
+		do_push(state, false);
+	print_stacks(state);
+	ft_printf("total moves: %d\n", state->moves);
+}
+
+void	randomize(t_state *state, int count, bool seed)
+{
+	if (seed)
+		srand(time(NULL));
+	fill_random(state->a, count);
+	empty_stack(state->b);
+	state->moves = 0;
+	begin(state);
+}
+
+void	test_find_cheapest(t_state *state)
+{
+	int		index;
+	t_node	*node;
+
+	index = find_cheapest(state);
+	if (index)
+	{
+		node = stack_get(state->a, index);
+		ft_printf(
+			"%s%d:\ttop %d, bottom %d%s\n", YEL, 
+			node->val, 
+			distance_from_top(state->b, node->val), 
+			distance_from_bottom(state->b, node->val), RST
+		);
+	}
 }
